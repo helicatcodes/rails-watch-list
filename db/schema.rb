0f69e69a2f842e593be41f9f3b846eb63d99caa4
Helena Wali <helicatsventures@gmail.com>
2026-03-05 15:26:35 +0100

    add movie list and bookmarks model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_05_141403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "movies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "overview"
+    t.string "poster_url"
+    t.float "rating"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 end
